91f05cfba1221ae09361a8b06abd87eedae4daa3
Nghia Hoang <nghiahoang880@gmail.com>
2026-06-21 18:44:38 +0700

    refactor(server): remove NestJS module structure after Express migration

--- server/src/employees/dto/update-employee.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateEmployeeDto } from './create-employee.dto.js';
-
-export class UpdateEmployeeDto extends PartialType(CreateEmployeeDto) {}
