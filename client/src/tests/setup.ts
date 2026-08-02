34a510a8053435430891339a79a679fba085bba0
Nghia Hoang <nghiahoang880@gmail.com>
2026-08-02 18:43:53 +0700

    test: mock matchMedia and ResizeObserver in setup

--- client/src/tests/setup.ts
+++ client/src/tests/setup.ts
@@ -5,3 +5,24 @@ import { server } from './mocks/server';
 beforeAll(() => server.listen({ onUnhandledRequest: 'warn' }));
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  }),
+});
+
+class ResizeObserverMock {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+Object.defineProperty(window, 'ResizeObserver', { writable: true, value: ResizeObserverMock });
